03a24e6bcc31c7f343354d11ecc331b852e85206
zer0dt <zer0dt@protonmail.com>
2024-12-18 14:47:32 -0500

    eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-declaration-merging": "off",
+      "prefer-const": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
